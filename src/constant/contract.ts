147c5a3eab3e56c88fcd8a001cb86f460467da50
codingAlphay <alpayaksumain@gmail.com>
2025-02-06 23:59:53 +0100

    Update ca

--- src/constant/contract.ts
+++ src/constant/contract.ts
@@ -1,15 +1,15 @@
 export const CONTRACT = {
   name: 'StarlightAI555',
   symbol: 'STAI',
-  ca: '0x0000000000000000000000000000000000000000',
+  ca: 'HQT9eGZ3vVREhzbkhnAMQ5NdQY4mDsCYP9FzY2SEV7x3',
   totalSupply: 1000000000,
   taxPercentage: 5,
   ownershipRenounced: true,
   description:
     'Starlight AI 555 ($STAI) is an AI-powered MemeCoin inspired by futuristic anime aesthetics, blending cutting-edge artificial intelligence with the vibrant world of anime culture. Featuring "Starlight AI 555," a digital anime goddess with an irresistible charm, this token brings a new wave of entertainment, innovation, and community engagement to the crypto space.',
   partners: [
-    { name: 'DEXView', url: 'https://www.dexview.com', imgUrl: '/dexview.svg' },
-    { name: 'DEXScreener', url: 'https://dexscreener.com/solana', imgUrl: '/dex-screener-seeklogo.svg' },
+    { name: 'DEXView', url: 'https://www.dexview.com/solana/HQT9eGZ3vVREhzbkhnAMQ5NdQY4mDsCYP9FzY2SEV7x3', imgUrl: '/dexview.svg' },
+    { name: 'DEXScreener', url: 'https://dexscreener.com/solana//HQT9eGZ3vVREhzbkhnAMQ5NdQY4mDsCYP9FzY2SEV7x3', imgUrl: '/dex-screener-seeklogo.svg' },
   ],
   telegram: 'https://x.com/ayyyyyy0850',
   twitter: 'https://x.com/ayyyyyy0850',
